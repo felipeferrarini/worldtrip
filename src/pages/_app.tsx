623a78080a430612c5c581f15b42c36f7e39feb2
Felipe de Abreu Ferrarini <ferrarinifelipe@gmail.com>
2021-10-18 18:20:52 -0300

    feat: added nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Header } from 'components/Header';
 import { theme } from 'styles/theme';
+import NextNProgress from 'nextjs-progressbar';
 
 import 'swiper/swiper-bundle.css';
 import '../styles/global.css';
@@ -10,6 +11,12 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ChakraProvider theme={theme}>
       <Header />
+      <NextNProgress
+        color={theme.colors.yellow[500]}
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   );
