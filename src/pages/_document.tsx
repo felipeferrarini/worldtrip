072a69c675bda11eb23c66e05c53aaea3c166a92
Felipe de Abreu Ferrarini <ferrarinifelipe@gmail.com>
2021-10-18 18:21:18 -0300

    chore: improve seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,17 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Barlow:wght@500;600&family=Poppins:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <meta name="title" content="WorldTrip" />
+          <meta property="og:locale" content="pt_BR" />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content="WorldTrip" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image:width" content="1200" />
+          <meta name="twitter:image:height" content="620" />
         </Head>
         <body>
           <Main />
